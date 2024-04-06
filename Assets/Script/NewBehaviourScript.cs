29877f342bfb319e9d7c82c6aeb46e41bbfbd5e9
Dao <tadangdao9120003@gmail.com>
2024-04-07 00:31:02 +0700

    init

---
+++ Assets/Script/NewBehaviourScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private PlayerInput input;
+    public void Update()
+    {
+        
+    }
+}
